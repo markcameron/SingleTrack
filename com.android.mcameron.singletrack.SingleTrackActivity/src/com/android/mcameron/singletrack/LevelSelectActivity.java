00a8b28e43aa63b21122e18c3fbdaf64cb851bd7
Mark Cameron <mark.cameron@sr.tamedia.ch>
2012-10-04 14:19:22 +0200

    Changed Buttons to an image-less system, defined in XML. Added buttons to GridView to improve look

--- com.android.mcameron.singletrack.SingleTrackActivity/src/com/android/mcameron/singletrack/LevelSelectActivity.java
+++ com.android.mcameron.singletrack.SingleTrackActivity/src/com/android/mcameron/singletrack/LevelSelectActivity.java
@@ -1,12 +1,16 @@
 package com.android.mcameron.singletrack;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 
 public class LevelSelectActivity extends Activity {
@@ -26,21 +30,84 @@ public class LevelSelectActivity extends Activity {
 	
 		gridView = (GridView) findViewById(R.id.grid);
 	
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, numbers);
+		gridView.setAdapter(new ButtonAdapter(this));	
+	}
 	
-		gridView.setAdapter(adapter);
-	
-		gridView.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View v,
-				int position, long id) {
-				Intent StartGameIntent = new Intent(LevelSelectActivity.this, SingleTrackActivity.class);
+	/**
+	 * An adapter for my button so that I can place it inside the custom GridView
+	 * 
+	 * @author markcameron
+	 */
+	public class ButtonAdapter extends BaseAdapter {
+		private Context mContext; 
+		
+		// Gets the context so it can be used later  
+		public ButtonAdapter(Context c) {  
+			mContext = c;  
+		}  
+		 
+		@Override
+		public int getCount() {
+			return numbers.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Button btn;  
+			if (convertView == null) {  
+				// if it's not recycled, initialize some attributes  
+				btn = new Button(mContext);  
+				// Set the font face because I cannot pass it to the Button constructor
+				btn.setTextSize(30f);
+				btn.setGravity(0x11);
+				btn.setTextColor(Color.WHITE);
+				btn.setTypeface(null, Typeface.BOLD);
+			}  
+			else {  
+				btn = (Button) convertView;  
+			}  
+
+			btn.setText(numbers[position]);
+			// Set the button to use my custom black background
+			btn.setBackgroundResource(R.drawable.btn_black);  
+			btn.setId(position);
+			
+		    // Set the onclicklistener so that pressing the button fires an event  
+		    // We will need to implement this onclicklistner.  
+		    btn.setOnClickListener(new MyOnClickListener(position));  
+	  
+			return btn;
+		}
+		
+		/**
+		 * OnClickListener for the button clicks inside the Custom GridView
+		 * 
+		 * @author markcameron
+		 */
+	    class MyOnClickListener implements OnClickListener {  
+	    	private final int position;  
+  
+	    	public MyOnClickListener(int position) {  
+	    		this.position = position;  
+	    	}
+  
+	    	public void onClick(View v) {  
+	    		// Open the level corresponding to the level selected in the GridView
+	    		Intent StartGameIntent = new Intent(LevelSelectActivity.this, SingleTrackActivity.class);
 				StartGameIntent.putExtra("LEVEL_ID", numbers[position]);
         		startActivity(StartGameIntent);
-			}
-		});
-	
+	    	}  
+	    }  
 	}
 
 }// class
