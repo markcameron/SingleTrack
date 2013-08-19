0c9be18354d5a2aa00d7e7d20dd33c55e5e64135
Mark Cameron <mark.oliver.cameron@gmail.com>
2013-08-19 23:34:46 +0200

    LEVELS: Added 7 new rectangle levels

--- com.android.mcameron.singletrack.SingleTrackActivity/src/com/android/mcameron/singletrack/LevelPackRectangles.java
+++ com.android.mcameron.singletrack.SingleTrackActivity/src/com/android/mcameron/singletrack/LevelPackRectangles.java
@@ -8,10 +8,16 @@ public class LevelPackRectangles extends Levels {
 		
 		levelList.add(null); //Level 0 - Doesn't exist - saves on silly - 1 operations
 		// 1-10
-		levelList.add(new float[]{2,2,0,0,0,1,1,1,0,1,1,0,0,0});
-		levelList.add(new float[]{3,3,0,2,1,1,0,2,0,1,1,2,1,1,1,0,2,0,2,1,2,0});
-		levelList.add(new float[]{3,3,0,2,2,0,0,2,0,1,1,0,0,0,1,1,1,0,1,1,1,2,2,0,2,1,2,2,2,1});
-		
+		levelList.add(new float[]{2,4,0,3,0,0,1,3,0,3,1,0,0,0,0,1,1,1,0,2,1,2});
+		levelList.add(new float[]{3,4,1,2,1,1,1,2,0,2,1,1,0,1,2,3,1,3,2,0,1,0});
+		levelList.add(new float[]{3,4,0,2,2,3,0,3,0,2,2,2,2,3,1,1,0,1});
+		levelList.add(new float[]{3,4,2,1,0,2,0,3,0,2,2,1,2,0,1,2,1,1});
+		levelList.add(new float[]{3,5,2,2,2,0,2,3,2,2,2,1,2,0,1,3,1,2,1,2,0,2});
+		levelList.add(new float[]{3,5,2,4,1,3,1,4,2,4,1,3,2,3,1,2,1,1,1,0,0,0,2,2,2,1});
+		levelList.add(new float[]{4,5,0,4,1,4,1,4,1,3,0,4,0,3,2,3,1,3,2,2,1,2,0,2,0,1,2,2,2,1,3,2,3,1});
+
+
+
 		return levelList;
 	}
 }
